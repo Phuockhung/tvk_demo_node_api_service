9288a0d58f393a2a7b448f504bf24ee8c6b4df38
Phuoc Nguyen <nguyendinhphuoc.cgl@gmail.com>
2022-02-28 14:35:54 +0700

    Update Antd Register

--- client/src/components/auth/LoginPass.tsx
+++ client/src/components/auth/LoginPass.tsx
@@ -2,80 +2,25 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { InputChange, FormSubmit } from "../../utils/TypeScript";
 import { login } from "../../redux/actions/authAction";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button } from "antd";
 
 const LoginPass = () => {
   const initialState = { account: "", password: "" };
   const [userLogin, setUserLogin] = useState(initialState);
   const { account, password } = userLogin;
-
-  const [typePass, setTypePass] = useState(false);
-
   const dispatch = useDispatch();
-
   const handleChangeInput = (e: InputChange) => {
     const { value, name } = e.target;
     setUserLogin({ ...userLogin, [name]: value });
   };
-
-  const handleSubmit = (e: FormSubmit) => {
-    e.preventDefault();
-    dispatch(login(userLogin));
-  };
-
   const onFinish = () => {
     dispatch(login(userLogin));
   };
 
   return (
-    // <form onSubmit={handleSubmit}>
-    //   <div className="form-group mb-3">
-    //     <label htmlFor="account" className="form-label">
-    //       Email / Phone number
-    //     </label>
-
-    //     <input
-    //       type="text"
-    //       className="form-control"
-    //       id="account"
-    //       name="account"
-    //       value={account}
-    //       onChange={handleChangeInput}
-    //     />
-    //   </div>
-
-    //   <div className="form-group mb-3">
-    //     <label htmlFor="password" className="form-label">
-    //       Password
-    //     </label>
-
-    //     <div className="pass">
-    //       <input
-    //         type={typePass ? "text" : "password"}
-    //         className="form-control"
-    //         id="password"
-    //         name="password"
-    //         value={password}
-    //         onChange={handleChangeInput}
-    //       />
-
-    //       <small onClick={() => setTypePass(!typePass)}>
-    //         {typePass ? "Hide" : "Show"}
-    //       </small>
-    //     </div>
-    //   </div>
-
-    //   <button
-    //     type="submit"
-    //     className="btn btn-dark w-100 mt-1"
-    //     disabled={account && password ? false : true}
-    //   >
-    //     Login
-    //   </button>
-    // </form>
     <Form layout="vertical" autoComplete="on" onFinish={onFinish}>
       <Form.Item
-        label="Username"
+        label="Email / Phone number"
         name="account"
         rules={[{ required: true, message: "Please input your username!" }]}
       >
--- client/src/components/auth/RegisterForm.tsx
+++ client/src/components/auth/RegisterForm.tsx
@@ -1,96 +1,69 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-
-import { InputChange, FormSubmit } from '../../utils/TypeScript'
-import { register } from '../../redux/actions/authAction'
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { InputChange, FormSubmit } from "../../utils/TypeScript";
+import { register } from "../../redux/actions/authAction";
+import { Form, Input, Button } from "antd";
 
 const RegisterForm = () => {
-
-  const initialState = { 
-    name: '', account: '', password: '', cf_password: '' 
-  }
-  const [userRegister, setUserRegister] = useState(initialState)
-  const { name, account, password, cf_password } = userRegister
-
-  const [typePass, setTypePass] = useState(false)
-  const [typeCfPass, setTypeCfPass] = useState(false)
-
-  const dispatch = useDispatch()
-
+  const initialState = {
+    name: "",
+    account: "",
+    password: "",
+    cf_password: "",
+  };
+  const [userRegister, setUserRegister] = useState(initialState);
+  const { name, account, password, cf_password } = userRegister;
+  const dispatch = useDispatch();
   const handleChangeInput = (e: InputChange) => {
-    const {value, name} = e.target
-    setUserRegister({...userRegister, [name]:value})
-  }
-
-  const handleSubmit = (e: FormSubmit) => {
-    e.preventDefault()
-    dispatch(register(userRegister))
-  }
+    const { value, name } = e.target;
+    setUserRegister({ ...userRegister, [name]: value });
+  };
+  const onFinish = () => {
+    dispatch(register(userRegister));
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group mb-3">
-        <label htmlFor="name" className="form-label">Name</label>
+    <Form layout="vertical" autoComplete="on" onFinish={onFinish}>
+      <Form.Item
+        label="Name"
+        name="name"
+        rules={[{ required: true, message: "Your name is up to 20 chars." }]}
+      >
+        <Input name="name" onChange={handleChangeInput} />
+      </Form.Item>
+      <Form.Item
+        label="Email / Phone number"
+        name="account"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input name="account" onChange={handleChangeInput} />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password name="password" onChange={handleChangeInput} />
+      </Form.Item>
+      <Form.Item
+        label="Confirm Password"
+        name="cf_password"
+        rules={[{ required: true, message: "Your confirm password." }]}
+      >
+        <Input.Password name="cf_password" onChange={handleChangeInput} />
+      </Form.Item>
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          disabled={name && account && password && cf_password ? false : true}
+          block
+        >
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
-        <input type="text" className="form-control" id="name"
-        name="name" value={name} onChange={handleChangeInput}
-        placeholder="Your name is up to 20 chars." />
-      </div>
-
-      <div className="form-group mb-3">
-        <label htmlFor="account" className="form-label">
-          Email / Phone number
-        </label>
-
-        <input type="text" className="form-control" id="account"
-        name="account" value={account} onChange={handleChangeInput}
-        placeholder="Example@gmail.com/+84123456789" />
-      </div>
-
-      <div className="form-group mb-3">
-        <label htmlFor="password" className="form-label">Password</label>
-
-        <div className="pass">
-          <input type={typePass ? "text" : "password"} 
-          className="form-control" 
-          id="password"
-          name="password" value={password} 
-          onChange={handleChangeInput} 
-          placeholder="Password must be at least 6 chars."
-          />
-
-          <small onClick={() => setTypePass(!typePass)}>
-            {typePass ? 'Hide' : 'Show'}
-          </small>
-        </div>
-      </div>
-
-      <div className="form-group mb-3">
-        <label htmlFor="password" className="form-label">
-          Confirm Password
-        </label>
-
-        <div className="pass">
-          <input type={typeCfPass ? "text" : "password"} 
-          className="form-control" 
-          id="cf_password"
-          name="cf_password" value={cf_password} 
-          onChange={handleChangeInput} 
-          placeholder="Your confirm password."
-          />
-
-          <small onClick={() => setTypeCfPass(!typeCfPass)}>
-            {typeCfPass ? 'Hide' : 'Show'}
-          </small>
-        </div>
-      </div>
-
-      <button type="submit" className="btn btn-dark w-100 my-1">
-        Register
-      </button>
-    </form>
-  )
-}
-
-export default RegisterForm
+export default RegisterForm;
