60b882d4b1088fdd56b535bb059668cde969b5f5
Phuoc Nguyen <nguyendinhphuoc.cgl@gmail.com>
2022-03-06 14:03:10 +0700

    Add send email footer

--- client/src/components/global/AppFooter.tsx
+++ client/src/components/global/AppFooter.tsx
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom";
-
 const AppFooter = () => {
   return (
     <div className="footer">
       <h6>Welcome my Blog</h6>
-      <Link to={`/`} target="_blank" rel="noreferrer" className="mb-2 d-block">
-        https://demo-blog-tvk.herokuapp.com/
-      </Link>
+      <a href={"mailto:phuocnd.ryan@gmail.com"}>
+        Email: Phuocnd.ryan@gmail.com
+      </a>
 
       <div className="copyright">Copyright &copy; 2022</div>
     </div>
